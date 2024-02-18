38d7d231c43992c34f73f8cbb2e9c512deefbab6
Andrey Krainyak <mode.andrew@gmail.com>
2024-02-18 09:24:21 +0800

    fix: rust-analyzer invalidates build cache

--- shell.nix
+++ shell.nix
@@ -4,7 +4,10 @@ pkgs.mkShell {
   packages = with pkgs; [
     cargo
     rustc
-    rust-analyzer
+    (writeShellScriptBin "rust-analyzer" ''
+      export CARGO_TARGET_DIR=.rust-analyzer
+      exec ${rust-analyzer}/bin/rust-analyzer "$@"
+    '')
     (python3.withPackages (ps: with ps; [
       pip
       pylint
